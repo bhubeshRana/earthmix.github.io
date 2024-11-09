89236f9a2c5dc03efb782e4245c29db758c44b03
bhubeshRana <bhubeshrana@gmail.com>
2024-11-09 22:04:58 +0530

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,76 @@
 // import Image from "next/image";
 import styles from "./page.module.css";
-
+import image1 from "../../public/1.jpeg"
+import image2 from "../../public/2.jpeg"
+import image3 from "../../public/3.jpeg"
+import image4 from "../../public/4.jpeg"
+import image5 from "../../public/5.jpeg"
+import image6 from "../../public/6.jpeg"
+import image7 from "../../public/7.jpeg"
+import image8 from "../../public/8.jpeg"
+import image9 from "../../public/9.jpeg"
+import image10 from "../../public/10.jpeg"
+import image11 from "../../public/11.jpeg"
+import image12 from "../../public/12.jpeg"
+import image13 from "../../public/13.jpeg"
+import image14 from "../../public/14.jpeg"
+import image15 from "../../public/15.jpeg"
+import image16 from "../../public/16.jpeg"
+import image17 from "../../public/17.jpeg"
+import image18 from "../../public/18.jpeg"
+import image19 from "../../public/19.jpeg"
 export default function Home() {
   return (
-    <div className={styles.page}>
-      This is earthmix new test
+    <div className="main-header">
+    
+<img src={image1.src}></img>
+<img src={image2.src}></img>
+<img src={image3.src}></img>
+<img src={image4.src}></img>
+<img src={image5.src}></img>
+<img src={image6.src}></img>
+<img src={image7.src}></img>
+<img src={image8.src}></img>
+<img src={image9.src}></img>
+<img src={image10.src}></img>
+<img src={image11.src}></img>
+<img src={image12.src}></img>
+<img src={image13.src}></img>
+<img src={image14.src}></img>
+<img src={image15.src}></img>
+<img src={image16.src}></img>
+<img src={image17.src}></img>
+<img src={image18.src}></img>
+<img src={image19.src}></img>
+
+      <nav className="navbar">
+        <ul className="menu">
+          <li><a href="#home">Home</a><ul className="menu">
+            <li><a href="#home">Home1</a></li>
+            <li><a href="#about">Home2</a></li>
+            <li><a href="#services">Home3</a></li>
+            <li><a href="#contact">Home4</a></li>
+          </ul></li>
+          <li><a href="#about">About</a><ul className="menu">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul></li>
+          <li><a href="#services">Services</a><ul className="menu">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul></li>
+          <li><a href="#contact">Contact</a><ul className="menu">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul></li>
+        </ul>
+      </nav>
       {/* <main className={styles.main}>
         <Image
           className={styles.logo}
