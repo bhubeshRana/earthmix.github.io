1d2aa2d61349ca95c3ddadb959c04f730595f218
bhubeshRana <bhubeshrana@gmail.com>
2024-11-09 22:07:19 +0530

    test

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"AkNWDKuecsAx4gGE/ehMl4sIpMy3uVi7wZPGblX7uAg=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
